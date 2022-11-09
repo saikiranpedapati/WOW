84eaa424b1dacbcb539517c03c481fe8cfd28f2d
saikiranpedapati <saikiran.sai619@gmail.com>
2022-11-10 03:53:28 +0530

    second commit regsitration page database connection

--- Wow1/app/src/main/java/com/example/wow1/MainActivity.java
+++ Wow1/app/src/main/java/com/example/wow1/MainActivity.java
@@ -1,29 +1,163 @@
 package com.example.wow1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+
+//    private FirebaseAuth auth;
+//    private EditText signupEmail, signupPassword;
+//    private Button button1;
+    DatabaseReference databaseReference;
+    DataSnapshot snapshot;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button bt2;
-        bt2=findViewById(R.id.button2);
+        final EditText phone=findViewById(R.id.editTextPhone2);
+        final EditText password=findViewById(R.id.editTextTextPassword);
+        final Button login=findViewById(R.id.button1);
+        final Button newuser=findViewById(R.id.button2);
 
+        Button bt2=findViewById(R.id.button2);
         bt2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,NewUser.class);
-                startActivity(intent);
-                finish();
+                startActivity(new Intent(MainActivity.this,NewUser.class));
             }
         });
-    }
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String phonetxt=phone.getText().toString();
+                final String passwordtxt=password.getText().toString();
+
+                if(phonetxt.isEmpty() || passwordtxt.isEmpty()){
+                    Toast.makeText(MainActivity.this,"please enter your mobile or password",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if(snapshot.hasChild(phonetxt)){
+                                final String getpassword=snapshot.child(phonetxt).child("password").getValue(String.class);
+
+                                if(getpassword.equals(passwordtxt)){
+                                    startActivity(new Intent(MainActivity.this,AfterSuccessfulLogin.class));
+                                }
+                            }
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////        buttontonextpage
+//
+//        Button bt2;
+//        bt2=findViewById(R.id.button2);
+//        bt2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,NewUser.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
+////                authorisation
+//                auth=FirebaseAuth.getInstance();
+//                signupEmail=findViewById(R.id.editTextTextEmailAddress);
+//                signupPassword=findViewById(R.id.editTextTextPassword);
+//                button1=findViewById(R.id.button1);
+//
+//                button1.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                        String user=signupEmail.getText().toString().trim();
+//                        String pass=signupPassword.getText().toString().trim();
+//
+//                        if(user.isEmpty()){
+//                            signupEmail.setError("Please Enter Email");
+//                        }
+//
+//                        if(user.isEmpty()){
+//                            signupPassword.setError("Please Enter Password");
+//                        }
+//
+//                        else{
+//                            auth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                                @Override
+//                                public void onComplete(@NonNull Task<AuthResult> task) {
+//                                    if(task.isSuccessful()){
+//                                        Toast.makeText(MainActivity.this,"signup successful",Toast.LENGTH_SHORT).show();
+//                                        startActivity(new Intent(MainActivity.this,AfterSuccessfulLogin.class));//
+//                                    }
+//                                    else{
+//                                        Toast.makeText(MainActivity.this,"incorrect username or password"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+//                                    }
+//
+//                                }
+//                            });
+//                        }
+//
+//
+//                    }
+//                });
+
+
+
+
+
+            }
+
+
 }
--- Wow1/app/src/main/java/com/example/wow1/NewUser.java
+++ Wow1/app/src/main/java/com/example/wow1/NewUser.java
@@ -1,19 +1,127 @@
 package com.example.wow1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class NewUser extends AppCompatActivity {
+
+    EditText personname, email,phone,password;
+//    DatabaseReference databaseReference;
+
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://wow1-faccd-default-rtdb.firebaseio.com/").child("getdata");
+
+//    private FirebaseAuth auth;
+//    private EditText loginEmail,LoginPassword;
+//    private Button button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
 
+//
+//        auth=FirebaseAuth.getInstance();
+//        loginEmail=findViewById(R.id.);
+
+        personname=findViewById(R.id.editTextTextPersonName);
+        email=findViewById(R.id.editTextTextEmailAddress2);
+        phone=findViewById(R.id.editTextPhone);
+        password=findViewById(R.id.editTextTextPassword2);
+
+        Button button3=findViewById(R.id.button3);
+
+//        databaseReference=FirebaseDatabase.getInstance().getReference().child("getdata");
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String personnametxt=personname.getText().toString();
+                String emailtxt=email.getText().toString();
+                String phonetxt=phone.getText().toString();
+                String passwordtxt=password.getText().toString();
+
+                if(personnametxt.isEmpty() ||emailtxt.isEmpty() ||phonetxt.isEmpty() ||passwordtxt.isEmpty()){
+
+                    Toast.makeText(NewUser.this,"enter all details",Toast.LENGTH_SHORT).show();
+                }
+
+
+//                else{
+//
+//                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                            if(snapshot.hasChild(phonetxt)){
+//                                Toast.makeText(NewUser.this,"user already registered",Toast.LENGTH_SHORT).show();
+//                            }
+//                            else{
+//                                databaseReference.child("users").child(phonetxt).child("personname").setValue(personnametxt);
+//                                databaseReference.child("users").child(phonetxt).child("email").setValue(emailtxt);
+//                                databaseReference.child("users").child(phonetxt).child("password").setValue(passwordtxt);
+//
+//                                Toast.makeText(NewUser.this,"successfully registered",Toast.LENGTH_SHORT).show();
+//                                finish();
+//
+//
+//                            }
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError error) {
+//
+//                        }
+//                    });
+
+//                    databaseReference.child("users").child(phonetxt).child("personname").setValue(personnametxt);
+//                    databaseReference.child("users").child(phonetxt).child("email").setValue(emailtxt);
+//                    databaseReference.child("users").child(phonetxt).child("password").setValue(passwordtxt);
+//
+//                    Toast.makeText(NewUser.this,"successfully registered",Toast.LENGTH_SHORT).show();
+//                    finish();
+
+//                }
+
+                else{
+                    System.out.println("happy");
+                    Toast.makeText(NewUser.this,"successfully registered",Toast.LENGTH_SHORT).show();
+
+
+                        String name=personname.getText().toString();
+                        String mail=email.getText().toString();
+                        String phoneno=phone.getText().toString();
+                        String pass=password.getText().toString();
+
+                        userLogin Logincred = new userLogin(name,mail,phoneno,pass);
+                        databaseReference.push().setValue(Logincred);
+
+
+                }
+
+
+            }
+        });
+
+
+
+
+//          backbuttontopreviouspage
         Button back;
         back=findViewById(R.id.button4);
 
@@ -25,6 +133,19 @@ public class NewUser extends AppCompatActivity {
                 finish();
             }
         });
+        //end
+
 
     }
+//    public void user()
+//    {
+//        String name=personname.getText().toString();
+//        String mail=email.getText().toString();
+//        String phoneno=phone.getText().toString();
+//        String pass=password.getText().toString();
+//
+//        userLogin Logincred = new userLogin(name,mail,phoneno,pass);
+//        databaseReference.push().setValue(Logincred);
+//
+//    }
 }
